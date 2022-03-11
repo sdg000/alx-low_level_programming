6426e99583fc89913111f2ca297d55cb7c03bba9
sdg000 <esec26@gmail.com>
2022-03-11 02:41:28 +0000

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 	float f;
 
 	printf("the size of an int is: %lu. \n", (unsigned long)sizeof(i));
-	printf("the size of a long int is: %lu. \n", (unsigned long)sizeof(l));
-	printf("the size of a long long int is: %lu. \n", (unsigned long)sizeof(y);
+	printf("the size of a long int is: %lu. \n", (unsigned long)sizeof(i2));
+	printf("the size of a long long int is: %lu. \n", (unsigned long)sizeof(i3);
 	printf("the size of a char is: %lu. \n", (unsigned long)sizeof(a));
 	printf("the size of a float is: %lu. \n", (unsigned long)sizeof(f));
 	return (0);
