61355bfd3e8f31be56f1e599ab0e5334e0b282b1
sdg000 <esec26@gmail.com>
2022-03-13 01:09:16 +0000

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,13 @@
 
 int main(void)
 {
-	int num;
+	int num = 48;
 
-	for (num = 0; num < 10; num++)
+	while (num < 58)
 	{
 	putchar(num);
+	num++;
 	}
-	printf('\n');
+	putchar('\n');
 	return (0);
 }
