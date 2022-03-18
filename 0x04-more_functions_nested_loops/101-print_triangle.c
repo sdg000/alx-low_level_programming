4de54cf75f144e3be223b210531658b6765e040c
sdg000 <esec26@gmail.com>
2022-03-18 18:43:30 +0000

    101-print_triangle.c

--- 0x04-more_functions_nested_loops/101-print_triangle.c
+++ 0x04-more_functions_nested_loops/101-print_triangle.c
@@ -7,5 +7,16 @@
 
 void print_number(int n)
 {
-	return (0);
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		x = -x;
+		_putchar(45);
+	}
+	if ((x / 10) > 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + 48);
 }
