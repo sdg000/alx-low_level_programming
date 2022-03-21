91f84a7b51b9e63c1a741de6f99ae046807b2815
sdg000 <esec26@gmail.com>
2022-03-21 21:38:41 +0000

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,16 +9,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	int length;
 
-	for (length = 0; src[length] != '\0'; length++)
-	{
-
-	}
-	
-	for (i = 0; i <= length; i++);
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
