bff282b90dc120803d3cb06be812d15c14f619db
sdg000 <esec26@gmail.com>
2022-03-21 21:50:45 +0000

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-int _atoi(char *s)
+int main(void)
 {
+	int b;
 
+	b = _atoi("98");
+	printf("%d\n", b);
+	b = _atoi("-402");
+	printf("%d\n", b);
+	b = _atoi("          ------++++++-----+++++--98");
+	printf("%d\n", b);
+	b = _atoi("214748364");
+	printf("%d\n", b);
+	b = _atoi("0");
+	printf("%d\n", b);
+	b = _atoi("Suite 402");
+	printf("%d\n", b);
+	b = _atoi("  +   +  -  -98 Battery Street; San Francisco, CA 94111 - USA  ");
+	printf("%d\n", b);
+	b = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	printf("%d\n", b);
 	return (0);
 }
