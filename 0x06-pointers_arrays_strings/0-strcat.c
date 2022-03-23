2dbf2c0b90cd16a3ef8c6f36f8db5640aea00056
sdg000 <esec26@gmail.com>
2022-03-23 21:27:28 +0000

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,16 @@
 * @src: character pointer variable
 * Return: pointer to the result sting dest
 */
-	char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int i, j = 0;
+int i, j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		while (src[j] != '\0')
-		{
-			dest[i + j] = src[j];
-			j++;
-		}
-	}
-	return (dest);
+for (i = 0; dest[i] != '\0'; i++)
+while (src[j] != '\0')
+{
+dest[i + j] = src[j];
+j++;
+}
+
+return (dest);
 }
