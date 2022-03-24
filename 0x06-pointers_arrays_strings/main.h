da02f9595dc8da21b84328e228917faefdaba19c
sdg000 <esec26@gmail.com>
2022-03-24 19:48:59 +0000

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 /* MAIN_H */
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,4 +9,3 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-int _putchar(char c);
