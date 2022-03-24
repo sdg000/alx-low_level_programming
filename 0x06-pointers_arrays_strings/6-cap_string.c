44ef4c0e39272f7bbae3286ace164622955b6738
sdg000 <esec26@gmail.com>
2022-03-24 18:46:44 +0000

    06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,19 +12,16 @@ int i, j;
 char sep[] = " \t\n,;.!?\"(){}";
 
 if (ch[0] == 'a' && ch[0] == 'z')
-{
 ch[0] = 'A' + (ch[0] - 'a');
 
 i = 1;
-}
+
 while (ch[i] != '\0')
-{
+
 for (j = 0; sep[j] != '\0'; j++)
-{
+
 if (ch[i - 1] == sep[j] && (ch[i] >= 'a' && ch[i] <= 'z'))
 ch[i] = 'A' + (ch[i] - 'a');
 i++;
-}
-}
 return (ch);
 }
