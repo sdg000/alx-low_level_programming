400538ec59105346bdef636fef0472e7cab8907a
sdg000 <esec26@gmail.com>
2022-03-24 19:12:08 +0000

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,21 +7,13 @@
 */
 char *cap_string(char *ch)
 {
-int i, j;
+	int	i;
 
-char sep[] = " \t\n,;.!?\"(){}";
-
-if (ch[0] == 'a' && ch[0] == 'z')
-ch[0] = 'A' + (ch[0] - 'a');
-
-i = 1;
-
-while (ch[i] != '\0')
-{
-for (j = 0; sep[j] != '\0'; j++)
-if (ch[i - 1] == sep[j] && (ch[i] >= 'a' && ch[i] <= 'z'))
-ch[i] = 'A' + (ch[i] - 'a');
-i++;
-}
-return (ch);
+	i = 0;
+	while (ch[i])
+	{
+		ch[i] = upper(ch[i]);
+		i++;
+	}
+	return (ch);
 }
