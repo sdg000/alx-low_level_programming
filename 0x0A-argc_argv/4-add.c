bc1cafb5129a930d17cc3a9ca9bfdb9c6c66a5e6
sdg000 <esec26@gmail.com>
2022-04-01 23:29:41 +0000

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,23 +7,21 @@
  * @argv: array pointing to arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, som = 0, a;
 
-	if (argc < 1)
+	if (argc == 1)
+	{
+		printf("0\n");
 		return (0);
-
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+		a = atoi(argv[i]);
+		if (a >= 0)
+			som = som + a;
 	}
-	printf("%d\n", sum);
-
+	printf("%d\n", som);
 	return (0);
 }
