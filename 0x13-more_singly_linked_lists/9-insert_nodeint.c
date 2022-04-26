dfea40b25b9f91f0ed2394a96272e434140d314d
sdg000 <esec26@gmail.com>
2022-04-26 23:02:59 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,45 @@
-#include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * insert_nodeint_at_index - entry point
- * @head: a list
- * @idx: position of new node
- * @n: new date
- * Return: new node or NULL if it fails.
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *ptr = *head, *save;
-	unsigned int cpt = 0;
+	listint_t *new, *temp = *head;
+	unsigned int con;
 
-	new =  malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-	new->next = NULL;
 
-	if (ptr == NULL || idx == 0)
+	if (idx == 0)
 	{
+		new->next = temp;
 		*head = new;
-		return (*head);
+		return (new);
 	}
 
-	while (cpt != idx - 1 && ptr->next != NULL)
+	for (con = 0; con < (idx - 1); con++)
 	{
-		ptr = ptr->next;
-		cpt++;
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
+
+		temp = temp->next;
 	}
 
-	if (ptr->next == NULL)
-		ptr->next = new;
-	else
-	{
-		save = ptr->next;
-		ptr->next = new;
-		new->next = save;
-	}
+	new->next = temp->next;
+	temp->next = new;
+
 	return (new);
 }
